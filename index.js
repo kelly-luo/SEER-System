fb4f6614baed3414e9ef19eb46b8ad3cd87850dd
BroYash <yashjeetbajwa8@gmail.com>
2020-08-06 11:01:05 +1200

    ss

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send("Test for pull request");
+  res.send('Hello World!');
 });
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-  console.log("Testing123");
 });
