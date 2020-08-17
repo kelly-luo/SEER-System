4e3dec3148f166babef7014f41a13e4811ffa271
BroYash <yashjeetbajwa8@gmail.com>
2020-08-17 20:58:26 +1200

    build

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://Hello:World@database.o473f.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(process.env.URI || 'mongodb+srv://Hello:World@database.o473f.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
