9efedea13d20f954147bab68f53bef7ee1ddcc12
kelly-luo <luo.kelly@hotmail.com>
2020-09-03 16:49:38 +1200

    Added custom search button

--- frontend/src/Components/pages/Homepage.js
+++ frontend/src/Components/pages/Homepage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Homepage.css';
 import NavigationBar from './NavBar'
 import SearchBar from './Searchbar'
-import { Button } from 'react-bootstrap'
+import { Button, Col, Row, Container } from 'react-bootstrap'
 
 function Homepage() {
     return (
@@ -11,7 +11,9 @@ function Homepage() {
             <div className='homepage_body'>
                 <img src="./SEER_logo.png" alt="" />
                 <SearchBar></SearchBar>
-                <Button variant="secondary">Custom Search</Button>{' '}
+                <div class="d-flex justify-content-center">
+                    <div class="p-3"><Button variant="secondary" size="sm">Custom Search</Button>{' '}</div>
+                </div>
             </div>
         </div>
     )
