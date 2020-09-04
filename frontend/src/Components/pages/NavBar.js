46229548a16f9a7510c4693dd44e23b9afdfbc49
kelly-luo <luo.kelly@hotmail.com>
2020-09-04 20:17:34 +1200

    Added nav bar to all existing pages

--- frontend/src/Components/pages/NavBar.js
+++ frontend/src/Components/pages/NavBar.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import {Link} from 'react-router-dom';
-import './Login.js'
+import './NavBar.css'
 
 class NavBar extends Component {
     render() {
         console.log("nav bar generated");
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <Link to ="/login" class="nav-link active">Sign in</Link>
+            <div className='navBar'>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <Link to ="/" class="nav-link active">Home</Link>
+                            <Link to ="/login" class="nav-link active">Sign in</Link>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         );
     }
 }
