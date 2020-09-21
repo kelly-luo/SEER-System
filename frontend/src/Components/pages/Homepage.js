c85f8217e6e0914298fe2834f893161b54b7ffd2
kelly-luo <luo.kelly@hotmail.com>
2020-09-22 11:40:02 +1200

    Ability to toggle visibility of the Custom Search

--- frontend/src/Components/pages/Homepage.js
+++ frontend/src/Components/pages/Homepage.js
@@ -5,15 +5,29 @@ import CustomSearchCard from './CustomSearchCard'
 import SearchBar from '../SearchComponents/Searchbar'
 import { Button } from 'react-bootstrap';
 
-function Homepage() {
+class Homepage extends React.Component{
 
-  const [formList, setFormList] = useState([])
+  constructor(props) {
+    super(props);
+    this.state = { customActive: false};
 
-  // const Card = () => {
-  //   return <p>CustomSearchCard</p>
+  }
+
+  // const [formList, setFormList] = useState([]);
+  // const [hidden, isHidden] = useState(false);
+
+  // toggleVisibility() {
+  //   console.log("Visiblity is now: " + this.state)
+
+  //   const { opened } = this.state;
+  //   this.setState((prevState) => {
+  //     active: !prevState.active
+  //   });
+
+  //   // setFormList(true)
   // }
 
-  const addCustomForm = (e) => {
+  addCustomForm = (e) => {
     console.log("Added Form")
     // const info = {
     //   text: "hi"
@@ -23,33 +37,39 @@ function Homepage() {
     //   formList: [...prevState.formList, <CustomSearchCard seField="SE Field" operator="Operator"/>]
     // }))
 
-    setFormList([<CustomSearchCard seField="SE Field" operator="Operator"/>,  ...formList])
+    // setFormList([<CustomSearchCard seField="SE Field" operator="Operator"/>,  ...formList])
   }
 
-  return (
-    <div className="homepage">
-      <NavigationBar></NavigationBar>
-      <div className="homepage_body">
-        <img src="./SEER_logo.png" alt="" />
-        <SearchBar></SearchBar>
-        <div className="d-flex justify-content-center">
-          <div className="p-3">
-            <Button id="customButton" variant="secondary" size="sm" onClick={e => addCustomForm(e)}>
-              Custom Search
-            </Button>
+  render(){
+    const { customActive } = this.state
+
+    return (
+      <div className="homepage">
+        <NavigationBar></NavigationBar>
+        <div className="homepage_body">
+          <img src="./SEER_logo.png" alt="" />
+          <SearchBar></SearchBar>
+          <div className="d-flex justify-content-center">
+            <div className="p-3">
+              <Button id="customButton" variant="secondary" size="sm" onClick={() => this.setState({ customActive: !customActive })}>
+                Custom Search
+              </Button>
+            </div>
+          </div>
+          { customActive && (
+            <div className="customForm" className="d-flex justify-content-center">
+              <CustomSearchCard seField="SE Field" operator="Operator"></CustomSearchCard>
+            </div>
+          )}
+          <div>
+            {/* {e => {if(this.formlist){
+              this.formList.map(cards => <CustomSearchCard seField="SE Field" operator="Operator"/>)
+              }}} */}
           </div>
         </div>
-        <div className="customForm" className="d-flex justify-content-center">
-          <CustomSearchCard seField="SE Field" operator="Operator"></CustomSearchCard>
-        </div>
-        <div>
-          {/* {e => {if(this.formlist){
-            this.formList.map(cards => <CustomSearchCard seField="SE Field" operator="Operator"/>)
-            }}} */}
-        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 
