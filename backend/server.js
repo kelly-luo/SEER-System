d835dce9a461fb1a5abeefc0736e7e54ca84ba45
BroYash <yashjeetbajwa8@gmail.com>
2020-09-25 22:34:45 +1200

    x

--- backend/server.js
+++ backend/server.js
@@ -33,12 +33,10 @@ fileRoutes(app);
 
 if (process.env.NODE_ENV === 'production'){
 
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-
     app.use(express.static('frontend/build'));
 
     app.get("*", (req,res) =>{
-        res.sendFile(path.resolve(__dirname,"../frontend", "build", "index.html"));
+        res.sendFile(path.resolve("../frontend", "build", "index.html"));
     });
 }
 
