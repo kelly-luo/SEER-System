f56717dcc47028793a350235368a5a023070ce75
BroYash <yashjeetbajwa8@gmail.com>
2020-09-25 17:26:38 +1200

    x

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,12 @@ SeRoutes(app);
 fileRoutes(app);
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('../frontend/build'));
+    const __dirname = process.cwd();
+    app.use(express.static('frontend/build'));
 
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname,"../frontend", "build", "index.html"));
+    });
 }
 
 
