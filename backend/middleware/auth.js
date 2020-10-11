1b4a9ab1f2e903c0c5aac126b682079dddf1debf
BroYash <yashjeetbajwa8@gmail.com>
2020-10-11 16:12:19 +1300

    heroku testing

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,18 +4,18 @@ const keys = require("../config/keys");
 function auth(req, res, next) {
     const token = req.header('x-auth-token')
 
-    if (!token)
-        return res.status(401).json({ msg: 'No token, authorization denied' })
+    //if (!token)
+    //return res.status(401).json({ msg: 'No token, authorization denied' })
 
 
-    try {
-        const decoded = jwt.verify(token, keys.secretOrKey);
-        req.user = decoded;
-        next();
+    //try {
+    const decoded = jwt.verify(token, keys.secretOrKey);
+    req.user = decoded;
+    next();
 
-    } catch (e) {
-        res.status(400).json({ msg: 'token is not valid' })
-    }
+    //} catch (e) {
+    //  res.status(400).json({ msg: 'token is not valid' })
+    //}
 
 }
 
